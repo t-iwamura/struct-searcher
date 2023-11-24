2a2eacce9f387567e432824e819a76b5a3fef5aa
Taiki Iwamura <takki.0206@gmail.com>
2023-11-24 10:23:33 +0900

    Write lammps command file

--- struct_searcher/scripts/main.py
+++ struct_searcher/scripts/main.py
@@ -1,5 +1,6 @@
 import click
 
+from struct_searcher.fileio import create_lammps_command_file
 from struct_searcher.struct import create_sample_struct_file
 
 
@@ -10,8 +11,13 @@ def main() -> None:
 
 @main.command()
 @click.argument("g_max", type=float)
-def generate(g_max) -> None:
+@click.option("-p", "--potential_file", required=True, help="Path to mlp.lammps")
+def generate(g_max, potential_file) -> None:
     n_atom = 4
     lammps_struct_file_content = create_sample_struct_file(g_max, n_atom)
     with open("initial_structure", "w") as f:
         f.write(lammps_struct_file_content)
+
+    lammps_command_file_content = create_lammps_command_file(potential_file)
+    with open("in.lammps", "w") as f:
+        f.write(lammps_command_file_content)
