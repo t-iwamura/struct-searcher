deb8d33303e079f92358d8eda6ebc829ae5ada49
Taiki Iwamura <takki.0206@gmail.com>
2023-11-28 16:23:55 +0900

    Generate 1000 sample structures for all the compositions

--- struct_searcher/utils.py
+++ struct_searcher/utils.py
@@ -1,4 +1,7 @@
+from pathlib import Path
 from typing import List, Tuple
+
+OUTPUTS_DIR_PATH = Path.home() / "struct-searcher" / "data" / "outputs"
 
 
 def create_n_atom_tuples(n_atom: int) -> List[Tuple[int, int]]:
@@ -12,3 +15,24 @@ def create_n_atom_tuples(n_atom: int) -> List[Tuple[int, int]]:
     """
     n_atom_tuples = [(n, n_atom - n) for n in range(n_atom + 1)]
     return n_atom_tuples
+
+
+def create_formula_dir_path(
+    elements: Tuple[str, str], n_atom_for_each_element: Tuple[int, int]
+) -> Path:
+    """Create Path object of formula directory
+
+    Args:
+        elements (Tuple[str, str]): The elements included in system.
+        n_atom_for_each_element (Tuple[int, int]): The number of atoms for each element.
+
+    Returns:
+        Path: Path object of formula directory.
+    """
+    system_name = "-".join(elements)
+    n_atom_id = str(sum(n_atom_for_each_element)).zfill(2)
+    formula = "-".join(f"{e}{n}" for e, n in zip(elements, n_atom_for_each_element))
+    formula_dir_path = (
+        OUTPUTS_DIR_PATH / system_name / "multi-start" / f"n_atom_{n_atom_id}" / formula
+    )
+    return formula_dir_path
